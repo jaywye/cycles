ff67d7371feba9ee658a12510036c375501e9db7
jaywye <developer.yanga@gmail.com>
2017-03-15 15:32:15 -0500

    Add files via upload

--- cycles.js
+++ cycles.js
@@ -88,27 +88,5 @@
                     d.date = new Date(last.date.getFullYear(), last.date.getMonth(), last.date.getDate() + d.add);
                 }
             };
-
-
-            $scope.cycleDate = new Date();
-
-            $scope.cycles = [
-                { day: 1, name: 'Day 1', date: new Date($scope.cycleDate.getFullYear(), $scope.cycleDate.getMonth(), $scope.cycleDate.getDate() + 1) },
-                { day: 2, name: 'Day 2', date: new Date($scope.cycleDate.getFullYear(), $scope.cycleDate.getMonth(), $scope.cycleDate.getDate() + 2) },
-                { day: 3, name: 'Day 3', date: new Date($scope.cycleDate.getFullYear(), $scope.cycleDate.getMonth(), $scope.cycleDate.getDate() + 3) },
-                { day: 4, name: 'Day 4', date: new Date($scope.cycleDate.getFullYear(), $scope.cycleDate.getMonth(), $scope.cycleDate.getDate() + 4) },
-                { day: 5, name: 'Day 5', date: new Date($scope.cycleDate.getFullYear(), $scope.cycleDate.getMonth(), $scope.cycleDate.getDate() + 5) },
-                { day: 6, name: 'Day 6', date: new Date($scope.cycleDate.getFullYear(), $scope.cycleDate.getMonth(), $scope.cycleDate.getDate() + 6) },
-                { day: 7, name: 'Day 7', date: new Date($scope.cycleDate.getFullYear(), $scope.cycleDate.getMonth(), $scope.cycleDate.getDate() + 7) },
-                { day: 8, name: 'Day 8', date: new Date($scope.cycleDate.getFullYear(), $scope.cycleDate.getMonth(), $scope.cycleDate.getDate() + 8) },
-                { day: 9, name: 'Day 9', date: new Date($scope.cycleDate.getFullYear(), $scope.cycleDate.getMonth(), $scope.cycleDate.getDate() + 9) },
-                { day: 10, name: 'Day 10', date: new Date($scope.cycleDate.getFullYear(), $scope.cycleDate.getMonth(), $scope.cycleDate.getDate() + 10) }
-            ];
-
-            $scope.cycleDateChanged = function () {
-                $scope.cycles.forEach(function (cycle) {
-                    cycle.date = new Date($scope.cycleDate.getFullYear(), $scope.cycleDate.getMonth(), $scope.cycleDate.getDate() + cycle.day);
-                });
-            };
         }]);
 })(angular);
